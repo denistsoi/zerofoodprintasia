75e97eccbb8adc869512d609d962d9bb390a61d8
Denis Tsoi <denistsoi@gmail.com>
2021-05-03 22:34:16 +0800

    ADD: clean up markup and fix footer;

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => (
+  <footer className="relative bg-green px-4 sm:px-6 lg:px-8">
+    <>
+      <div className="max-w-prose mx-auto">
+        <div className="flex justify-between items-center py-2 md:space-x-10">
+          <div className="flex lg:w-0 lg:flex-1">
+            <span className="inline-flex items-center text-white text-sm">
+              Zero Footprint Asia ©2021
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  </footer>
+)
---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+export const Header = () => (
+  <header className="relative bg-green">
+    <>
+      <div className="max-w-8xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center py-2 md:justify-center md:space-x-10">
+          <div className="flex justify-center lg:w-0 lg:flex-1">
+            <a href="#" className="inline-flex items-center">
+              <img
+                className="h-16 w-auto sm:h-18"
+                src="./logo.png"
+                alt="zfpa"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  </header>
+)
